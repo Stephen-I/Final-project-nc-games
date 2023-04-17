20899280675974b40de79e03bc985eaddd4235ab
Stephen Irinoye <stephenirinoye@outlook.com>
2023-04-17 13:50:28 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <section className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Nc games</header>
+      <Routes>
+        <Route path="/api/categories" element={<CategoriesLink />}></Route>
+      </Routes>
     </section>
   );
 }
