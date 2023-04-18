c86727417638a5887e8ce5a75a1e7e510ede10ae
Stephen Irinoye <stephenirinoye@outlook.com>
2023-04-18 11:07:48 +0100

    ticket 5 edit

--- src/Components/SingleReview.jsx
+++ src/Components/SingleReview.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchSingleReview } from "../Api";
+import { Link } from "react-router-dom";
 
 const Review = () => {
   const { review_id } = useParams();
@@ -32,6 +33,7 @@ const Review = () => {
       />
       <p>{singleReview.review_body}</p>
       <p>{singleReview.created_at}</p> <p>votes: {singleReview.votes}</p>
+      <Link to="/">Home</Link> <Link to="/reviews">All Reviews</Link>;
     </main>
   );
 };
